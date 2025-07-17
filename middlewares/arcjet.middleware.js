22851a398282e8f8d0f7a94d9b96a5d015fbf6a2
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-17 22:59:04 +0530

    add arcjet middleware for advanced  bot detection and rate limiting

---
+++ middlewares/arcjet.middleware.js
@@ -0,0 +1,33 @@
+import { aj } from "../config/arcjet.js";
+
+export const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req);
+
+    if(decision.isDenied()){
+        if(decision.reason.isRateLimit()) {
+            res.status(429).json({
+                success: false,
+                message: "Rate limit exceeded",
+            });
+        }
+
+        if(decision.reason.isBot()) {
+            res.status(403).json({
+                success: false,
+                message: "Bots are not allowed",
+            });
+        }
+
+        return res.status(403).json({
+            success: false,
+            message: "Access denied",
+        });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Arcjet middleware error:", error);
+    next(error);
+  }
+};
