2a61a7433c551adc14c296d0b38b4922911b35ec
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-18 00:46:18 +0530

    update arcjet middleware logic

--- middlewares/arcjet.middleware.js
+++ middlewares/arcjet.middleware.js
@@ -2,27 +2,27 @@ import { aj } from "../config/arcjet.js";
 
 export const arcjetMiddleware = async (req, res, next) => {
   try {
-    const decision = await aj.protect(req);
+    const decision = await aj.protect(req, { requested: 1 });
 
-    if(decision.isDenied()){
-        if(decision.reason.isRateLimit()) {
-            res.status(429).json({
-                success: false,
-                message: "Rate limit exceeded",
-            });
-        }
-
-        if(decision.reason.isBot()) {
-            res.status(403).json({
-                success: false,
-                message: "Bots are not allowed",
-            });
-        }
-
-        return res.status(403).json({
-            success: false,
-            message: "Access denied",
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        res.status(429).json({
+          success: false,
+          message: "Rate limit exceeded",
         });
+      }
+
+      if (decision.reason.isBot()) {
+        res.status(403).json({
+          success: false,
+          message: "Bots are not allowed",
+        });
+      }
+
+      return res.status(403).json({
+        success: false,
+        message: "Access denied",
+      });
     }
 
     next();
