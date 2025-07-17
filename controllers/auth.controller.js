8aab8e14a5bfd78bed234b3ab59dd5df9d3e65ee
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-17 12:37:43 +0530

    update sign-up logic

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,10 +2,11 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET, JWT_EXPIRY } from "../config/env.js";
+import User from "../models/user.model.js";
 
 export const signUp = async (req, res, next) => {
-  const session = mongoose.startSession();
-  await session.startTransaction();
+  const session = await mongoose.startSession();
+  session.startTransaction();
 
   try {
     const { name, email, password } = req.body;
@@ -20,10 +21,9 @@ export const signUp = async (req, res, next) => {
     const hashedPassword = await bcrypt.hash(password, salt);
 
     const newUsers = await User.create(
-      { name, email, password: hashedPassword },
+      [{ name, email, password: hashedPassword }],
       { session }
     );
-    await newUsers.save();
 
     const token = jwt.sign({ userId: newUsers[0]._id }, JWT_SECRET, {
       expiresIn: JWT_EXPIRY,
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,16 +5,10 @@ const authRouter = Router();
 
 // /api/v1/auth
 // POST /api/v1/auth/sign-up
-authRouter.post("/sign-up", (req, res) => {
-  signUp;
-});
+authRouter.post("/sign-up", signUp);
 // POST /api/v1/auth/sign-in
-authRouter.post("/sign-in", (req, res) => {
-  signIn;
-});
+authRouter.post("/sign-in", signIn);
 // POST /api/v1/auth/sign-out
-authRouter.post("/sign-out", (req, res) => {
-  signOut;
-});
+authRouter.post("/sign-out", signOut);
 
 export default authRouter;
