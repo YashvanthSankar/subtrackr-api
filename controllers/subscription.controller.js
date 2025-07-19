c48d0c0839d14ed6a38576327bbfd09d99accb1f
Yashvanth S <yashvanthsankar@gmail.com>
2025-07-19 18:37:54 +0530

    update workflow logic for subscription renewal reminders

--- controllers/subscription.controller.js
+++ controllers/subscription.controller.js
@@ -1,10 +1,16 @@
+import { workflowClient } from "../config/upstash.js";
 import Subscription from "../models/subscription.model.js";
+import { workflowClient } from "../config/upstash.js";
 
 export const createSubscription = async (req, res, next) => {
   try {
     const subscription = await Subscription.create({
       ...req.body,
       user: req.user.id,
+    });
+
+    await workflowClient.trigger({
+      url: `${SERVER_URL}`,
     });
 
     res.status(201).json({
